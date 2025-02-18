5f20cdd19ee51916048b05973bb0998582d0cc38
Nathan <9020870@student.tcrmbo.nl>
2025-02-18 13:40:32 +0100

    change image of cookie

--- src/script.js
+++ src/script.js
@@ -1,23 +1,28 @@
+// Initialize the game variables
 let points = parseInt(localStorage.getItem("points")) || 0;
 let autoClickers = parseInt(localStorage.getItem("autoClickers")) || 0;
 let clickPower = parseInt(localStorage.getItem("clickPower")) || 1;
 
+// Update UI with initial values
 document.getElementById("points").innerText = points;
 document.getElementById("autoclickers").innerText = autoClickers;
 document.getElementById("clickPower").innerText = clickPower;
 
+// Save game data to localStorage
 function saveGame() {
     localStorage.setItem("points", points);
     localStorage.setItem("autoClickers", autoClickers);
     localStorage.setItem("clickPower", clickPower);
 }
 
+// Click on the cookie to increase points
 function clickCookie() {
     points += clickPower;
     document.getElementById("points").innerText = points;
     saveGame();
 }
 
+// Buy an auto-clicker
 function buyAutoClicker() {
     if (points >= 10) {
         points -= 10;
@@ -28,6 +33,7 @@ function buyAutoClicker() {
     }
 }
 
+// Buy an upgrade to increase click power
 function buyUpgrade() {
     if (points >= 50) {
         points -= 50;
@@ -38,11 +44,9 @@ function buyUpgrade() {
     }
 }
 
-
+// Automatically gain points based on auto-clickers
 setInterval(() => {
     points += autoClickers;
     document.getElementById("points").innerText = points;
     saveGame();
 }, 1000);
-
-document.getElementById("cookie").addEventListener("click", clickCookie);
