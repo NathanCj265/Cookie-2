ab967ef00200557c50a6ef26f1bcb9d6805ee06f
Nathan <9020870@student.tcrmbo.nl>
2025-03-03 13:19:37 +0100

    adding stats on profile

--- src/script.js
+++ src/script.js
@@ -1,64 +1,67 @@
-// Initialize data
 let points = parseInt(localStorage.getItem("points")) || 0;
 let autoClickers = parseInt(localStorage.getItem("autoClickers")) || 0;
 let clickPower = parseInt(localStorage.getItem("clickPower")) || 1;
+let autoClickerSpeed = [0, 1, 2, 3]; 
+let clickerNames = ["Cookie Clicker", "Double Clicker", "Triple Clicker"]; 
+let upgradeNames = ["Click Power", "Auto-Clicker Speed", "Point Multiplier"]; 
+let upgradePrices = [50, 200, 500]; 
+let autoClickerPrices = [10, 50, 200]; 
 
-// Set the display of initial data
 document.getElementById("points").innerText = points;
 document.getElementById("autoclickers").innerText = autoClickers;
 document.getElementById("clickPower").innerText = clickPower;
 
-// Auto-clicker upgrade costs and stats
-let autoclickerCosts = [10, 50, 100, 200, 500, 1000, 2000, 5000]; // Cost for each autoclicker upgrade
-let autoclickerPoints = [1, 2, 5, 10, 20, 50, 100, 200]; // Points per second for each autoclicker
 
-// Upgrade costs for unique upgrades
-let upgradeCosts = [50, 200, 500, 1000, 2500]; // Costs for click power upgrades
-let upgradeBoosts = [1, 2, 5, 10, 20]; // Boost for each upgrade
-
-// Save game data to localStorage
 function saveGame() {
     localStorage.setItem("points", points);
     localStorage.setItem("autoClickers", autoClickers);
     localStorage.setItem("clickPower", clickPower);
+    localStorage.setItem("autoClickerSpeed", JSON.stringify(autoClickerSpeed));
 }
 
-// Click cookie function
+
 function clickCookie() {
     points += clickPower;
     document.getElementById("points").innerText = points;
     saveGame();
 }
 
-// Auto-clicker purchase function
-function buyAutoClicker(index) {
-    if (points >= autoclickerCosts[index]) {
-        points -= autoclickerCosts[index];
-        autoClickers += autoclickerPoints[index];
+
+function buyAutoClicker(level) {
+    if (points >= autoClickerPrices[level - 1]) {
+        points -= autoClickerPrices[level - 1];
+        autoClickers++;
+        autoClickerSpeed[level]++;  
         document.getElementById("points").innerText = points;
         document.getElementById("autoclickers").innerText = autoClickers;
+        alert(`You bought a ${clickerNames[level - 1]}!`);
         saveGame();
     }
 }
 
-// Upgrade purchase function
-function buyUpgrade(index) {
-    if (points >= upgradeCosts[index]) {
-        points -= upgradeCosts[index];
-        clickPower += upgradeBoosts[index];
+
+function buyUpgrade(level) {
+    if (points >= upgradePrices[level - 1]) {
+        points -= upgradePrices[level - 1];
+        if (level === 1) {
+            clickPower++; 
+        } else if (level === 2) {
+            autoClickerSpeed[level - 1]++; 
+        } else if (level === 3) {
+            clickPower += 2; 
+        }
         document.getElementById("points").innerText = points;
         document.getElementById("clickPower").innerText = clickPower;
+        alert(`You upgraded ${upgradeNames[level - 1]}!`);
         saveGame();
     }
 }
 
-// Set interval for auto-clickers to generate points
+
 setInterval(() => {
-    points += autoClickers;
+    points += autoClickers * clickerNames.length;  
     document.getElementById("points").innerText = points;
     saveGame();
 }, 1000);
 
-// Event listener for cookie click
 document.getElementById("cookie").addEventListener("click", clickCookie);
-
