3f803d1720d780c01fdcbf98d49b01aa5d4f11be
Nathan <9020870@student.tcrmbo.nl>
2025-03-10 15:07:17 +0100

    Had trouble with my cookie clicker point kept saying object but now it is fixed.

--- src/script.js
+++ src/script.js
@@ -1,15 +1,92 @@
+let actionsTaken = 0;
+let totalScore = 0;
+
+class Button {
+    constructor(buttonHTMLID) {
+        document.getElementById(buttonHTMLID).addEventListener("click", () => this.handleClick());
+    }
+
+    handleClick() {
+        actionsTaken += 1;
+        this.buttonSpecificBehaviour();
+        updateScoreDisplay();
+    }
+
+    buttonSpecificBehaviour() {
+        throw new Error("Not implemented/abstract");
+    }
+}
+
+class Incr1Button extends Button {
+    buttonSpecificBehaviour() {
+        totalScore += 1;
+        console.log('Cookie clicked: Score increased by 1');
+    }
+}
+
+class AutoClickerButton extends Button {
+    constructor(buttonHTMLID, autoClickerType, cost, game) {
+        super(buttonHTMLID);
+        this.autoClickerType = autoClickerType;
+        this.cost = cost;
+        this.game = game;
+    }
+
+    buttonSpecificBehaviour() {
+        if (this.game.points >= this.cost) {
+            this.game.points -= this.cost;
+            this.game.autoClickers[this.autoClickerType]++;
+            console.log(`${this.autoClickerType} bought!`);
+            this.game.updateUI();
+        } else {
+            alert("Not enough points!");
+        }
+    }
+}
+
+class UpgradeButton extends Button {
+    constructor(buttonHTMLID, upgradeType, cost, game) {
+        super(buttonHTMLID);
+        this.upgradeType = upgradeType;
+        this.cost = cost;
+        this.game = game;
+    }
+
+    buttonSpecificBehaviour() {
+        if (this.game.points >= this.cost) {
+            this.game.points -= this.cost;
+            this.game.clickPower += this.game.upgrades[this.upgradeType].boost;
+            console.log(`${this.upgradeType} upgrade purchased!`);
+            this.game.updateUI();
+        } else {
+            alert("Not enough points!");
+        }
+    }
+}
+
+
 class CookieClickerGame {
     constructor() {
         this.points = parseInt(localStorage.getItem("points")) || 0;
-        this.autoClickers = new AutoClickers();
-        this.upgrades = new Upgrades();
         this.clickPower = parseInt(localStorage.getItem("clickPower")) || 1;
-
-        this.updateUI();
+        this.autoClickers = {
+            cursor: 0,
+            grandma: 0,
+            bakery: 0,
+            factory: 0,
+            mine: 0
+        };
+        this.upgrades = {
+            goldenFingers: { cost: 50, boost: 1 },
+            ironCookie: { cost: 250, boost: 5 },
+            cookieGod: { cost: 1000, boost: 10 },
+            cookieFactory: { cost: 5000, boost: 50 },
+            cookieEmpire: { cost: 10000, boost: 100 }
+        };
         this.startAutoClicker();
+        this.updateUI();
     }
 
-    
     saveGame() {
         localStorage.setItem("points", this.points);
         localStorage.setItem("clickPower", this.clickPower);
@@ -23,140 +100,48 @@ class CookieClickerGame {
         this.saveGame();
     }
 
-    
     startAutoClicker() {
         setInterval(() => {
-            this.autoClickers.applyAutoClickers(this);
+            for (let type in this.autoClickers) {
+                this.points += this.autoClickers[type];
+            }
             this.updateUI();
             this.saveGame();
-        }, 1000); 
+        }, 1000);
     }
 
-   
     updateUI() {
         document.getElementById("points").innerText = this.points;
-        this.autoClickers.updateUI();
-        this.upgrades.updateUI();
-    }
-}
+       
+        document.getElementById("autoclickers-0").innerText = `Cursor: ${this.autoClickers.cursor}`;
+        document.getElementById("autoclickers-1").innerText = `Grandma: ${this.autoClickers.grandma}`;
+        document.getElementById("autoclickers-2").innerText = `Bakery: ${this.autoClickers.bakery}`;
+        document.getElementById("autoclickers-3").innerText = `Factory: ${this.autoClickers.factory}`;
+        document.getElementById("autoclickers-4").innerText = `Mine: ${this.autoClickers.mine}`;
 
-
-class AutoClickers {
-    constructor() {
-        this.autoClickers = {
-            cursor: 0,
-            grandma: 0,
-            bakery: 0,
-            factory: 0,
-            mine: 0
-        };
-    }
-
-    buyAutoClicker(type, game) {
-        let cost = this.calculateCost(type);
-
-        if (game.points >= cost) {
-            game.points -= cost;
-            this.autoClickers[type]++;
-            game.updateUI();
-            game.saveGame();
-        } else {
-            alert("Not enough points!");
-        }
-    }
-
-    
-    calculateCost(type) {
-        let cost = 0;
-        switch (type) {
-
-            case "cursor":
-                cost = 10 + this.autoClickers.cursor * 5;
-                break;
-            case "grandma":
-                cost = 20 + this.autoClickers.grandma * 10;
-                break;
-            case "bakery":
-                cost = 100 + this.autoClickers.bakery * 50;
-                break;
-            case "factory":
-                cost = 500 + this.autoClickers.factory * 100;
-                break;
-            case "mine":
-                cost = 2000 + this.autoClickers.mine * 500;
-                break;  
-        }
-        return cost;
-    }
-
-   
-    applyAutoClickers(game) {
-        for (const key in this.autoClickers) {
-            game.points += this.autoClickers[key];
-        }
-    }
-
-    updateUI() {
-        document.getElementById("autoclickers-0").innerText = `Cursor: ${this.autoClickers.cursor} (Cost: ${10 + this.autoClickers.cursor * 5} pts)`;
-        document.getElementById("autoclickers-1").innerText = `Grandma: ${this.autoClickers.grandma} (Cost: ${20 + this.autoClickers.grandma * 10} pts)`;
-        document.getElementById("autoclickers-2").innerText = `Bakery: ${this.autoClickers.bakery} (Cost: ${100 + this.autoClickers.bakery * 50} pts)`;
-        document.getElementById("autoclickers-3").innerText = `Factory: ${this.autoClickers.factory} (Cost: ${500 + this.autoClickers.factory * 100} pts)`;
-        document.getElementById("autoclickers-4").innerText = `Mine: ${this.autoClickers.mine} (Cost: ${2000 + this.autoClickers.mine * 500} pts)`;
-    }
-
-}
-
-
-class Upgrades {
-    constructor() {
-        this.upgrades = {
-            goldenFingers: { cost: 50, boost: 1 },
-            ironCookie: { cost: 250, boost: 5 },
-            cookieGod: { cost: 1000, boost: 10 },
-            cookieFactory: { cost: 5000, boost: 50 },
-            cookieEmpire: { cost: 10000, boost: 100 }
-        };
-    }
-
-    
-    buyUpgrade(type, game) {
-        const upgrade = this.upgrades[type];
-
-        if (game.points >= upgrade.cost) {
-            game.points -= upgrade.cost;
-            game.clickPower += upgrade.boost;
-            game.updateUI();
-            game.saveGame();
-        } else {
-            alert("Not enough points!");
-        }
-    }
-
-    
-    updateUI() {
-        document.getElementById("upgrade-1").innerText = `Golden Fingers: +1 Click Power (Cost: ${this.upgrades.goldenFingers.cost} pts)`;
-        document.getElementById("upgrade-2").innerText = `Iron Cookie: +5 Click Power (Cost: ${this.upgrades.ironCookie.cost} pts)`;
-        document.getElementById("upgrade-3").innerText = `Cookie God: +10 Click Power (Cost: ${this.upgrades.cookieGod.cost} pts)`;
-        document.getElementById("upgrade-4").innerText = `Cookie Factory: +50 Click Power (Cost: ${this.upgrades.cookieFactory.cost} pts)`;
-        document.getElementById("upgrade-5").innerText = `Cookie Empire: +100 Click Power (Cost: ${this.upgrades.cookieEmpire.cost} pts)`;
+        document.getElementById("upgrade-1").innerText = `Golden Fingers: +1 Click Power (Cost: ${this.upgrades.goldenFingers.cost})`;
+        document.getElementById("upgrade-2").innerText = `Iron Cookie: +5 Click Power (Cost: ${this.upgrades.ironCookie.cost})`;
+        document.getElementById("upgrade-3").innerText = `Cookie God: +10 Click Power (Cost: ${this.upgrades.cookieGod.cost})`;
+        document.getElementById("upgrade-4").innerText = `Cookie Factory: +50 Click Power (Cost: ${this.upgrades.cookieFactory.cost})`;
+        document.getElementById("upgrade-5").innerText = `Cookie Empire: +100 Click Power (Cost: ${this.upgrades.cookieEmpire.cost})`;
     }
 }
 
 
 const game = new CookieClickerGame();
 
+
 document.getElementById("cookie").addEventListener("click", () => game.clickCookie());
 
-document.getElementById("buyCursor").addEventListener("click", () => game.autoClickers.buyAutoClicker("cursor", game));
-document.getElementById("buyGrandma").addEventListener("click", () => game.autoClickers.buyAutoClicker("grandma", game));
-document.getElementById("buyBakery").addEventListener("click", () => game.autoClickers.buyAutoClicker("bakery", game));
-document.getElementById("buyFactory").addEventListener("click", () => game.autoClickers.buyAutoClicker("factory", game));
-document.getElementById("buyMine").addEventListener("click", () => game.autoClickers.buyAutoClicker("mine", game));
+new Incr1Button("cookie");  
+new AutoClickerButton("buyCursor", "cursor", 10, game);
+new AutoClickerButton("buyGrandma", "grandma", 20, game);
+new AutoClickerButton("buyBakery", "bakery", 100, game);
+new AutoClickerButton("buyFactory", "factory", 500, game);
+new AutoClickerButton("buyMine", "mine", 2000, game);
 
-document.getElementById("buyGoldenFingers").addEventListener("click", () => game.upgrades.buyUpgrade("goldenFingers", game));
-document.getElementById("buyIronCookie").addEventListener("click", () => game.upgrades.buyUpgrade("ironCookie", game));
-document.getElementById("buyCookieGod").addEventListener("click", () => game.upgrades.buyUpgrade("cookieGod", game));
-document.getElementById("buyCookieFactory").addEventListener("click", () => game.upgrades.buyUpgrade("cookieFactory", game));
-document.getElementById("buyCookieEmpire").addEventListener("click", () => game.upgrades.buyUpgrade("cookieEmpire", game));
-
-
+new UpgradeButton("buyGoldenFingers", "goldenFingers", 50, game);
+new UpgradeButton("buyIronCookie", "ironCookie", 250, game);
+new UpgradeButton("buyCookieGod", "cookieGod", 1000, game);
+new UpgradeButton("buyCookieFactory", "cookieFactory", 5000, game);
+new UpgradeButton("buyCookieEmpire", "cookieEmpire", 10000, game);
