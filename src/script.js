a669210ce617a1d160364961a39f98231e26c9fd
Nathan <9020870@student.tcrmbo.nl>
2025-03-20 10:08:00 +0100

    more auto-clickers

--- src/script.js
+++ src/script.js
@@ -1,6 +1,3 @@
-let actionsTaken = 0;
-let totalScore = 0;
-
 class Button {
     constructor(buttonHTMLID) {
         const buttonElement = document.getElementById(buttonHTMLID);
@@ -12,9 +9,7 @@ class Button {
     }
 
     handleClick() {
-        actionsTaken += 1;
         this.buttonSpecificBehaviour();
-        updateScoreDisplay();
     }
 
     buttonSpecificBehaviour() {
@@ -23,9 +18,13 @@ class Button {
 }
 
 class Incr1Button extends Button {
+    constructor(buttonHTMLID, game) {
+        super(buttonHTMLID);
+        this.game = game;
+    }
+
     buttonSpecificBehaviour() {
-        totalScore += 1;
-        console.log('Cookie clicked: Score increased by 1');
+        this.game.clickCookie();
     }
 }
 
@@ -41,8 +40,8 @@ class AutoClickerButton extends Button {
         if (this.game.points >= this.cost) {
             this.game.points -= this.cost;
             this.game.autoClickers[this.autoClickerType]++;
-            console.log(`${this.autoClickerType} bought!`);
             this.game.updateUI();
+            this.game.saveGame();
         } else {
             alert("Not enough points!");
         }
@@ -61,8 +60,8 @@ class UpgradeButton extends Button {
         if (this.game.points >= this.cost) {
             this.game.points -= this.cost;
             this.game.clickPower += this.game.upgrades[this.upgradeType].boost;
-            console.log(`${this.upgradeType} upgrade purchased!`);
             this.game.updateUI();
+            this.game.saveGame();
         } else {
             alert("Not enough points!");
         }
@@ -81,8 +80,8 @@ class ThemesButton extends Button {
         if (this.game.points >= this.cost) {
             this.game.points -= this.cost;
             this.applyTheme(this.themeType);
-            console.log(`${this.themeType} theme purchased!`);
             this.game.updateUI();
+            this.game.saveGame();
         } else {
             alert("Not enough points!");
         }
@@ -95,37 +94,48 @@ class ThemesButton extends Button {
     }
 }
 
-class saveGameButton extends Button {
-    buttonSpecificBehaviour() {
-        localStorage.setItem("totalScore", totalScore);
-        localStorage.setItem("actionsTaken", actionsTaken);
-        console.log('Game saved!');
-    }
-}
 class CookieClickerGame {
     constructor() {
+        this.loadGame();
+        this.startAutoClicker();
+        this.startClicksPerSecondCounter();
+        this.startSpecialEvents();
+        this.updateUI();
+    }
+
+    saveGame() {
+        localStorage.setItem("points", this.points);
+        localStorage.setItem("clickPower", this.clickPower);
+        localStorage.setItem("clicks", this.clicks);
+        localStorage.setItem("clicksPerSecond", this.clicksPerSecond);
+        localStorage.setItem("autoClickers", JSON.stringify(this.autoClickers));
+        localStorage.setItem("upgrades", JSON.stringify(this.upgrades));
+        localStorage.setItem("themes", JSON.stringify(this.themes));
+    }
+
+    loadGame() {
         this.points = parseInt(localStorage.getItem("points")) || 0;
         this.clickPower = parseInt(localStorage.getItem("clickPower")) || 1;
         this.clicks = parseInt(localStorage.getItem("clicks")) || 0;
-        this.clicksPerSecond = 0;
-        this.autoClickers = {
+        this.clicksPerSecond = parseInt(localStorage.getItem("clicksPerSecond")) || 0;
+        this.autoClickers = JSON.parse(localStorage.getItem("autoClickers")) || {
             cursor: 0,
             grandma: 0,
             bakery: 0,
             factory: 0,
             mine: 0,
-            spaceship: 0, // New auto-clicker
-            robot: 0, // New auto-clicker
-            alien: 0 // New auto-clicker
+            spaceship: 0,
+            robot: 0,
+            alien: 0
         };
-        this.upgrades = {
+        this.upgrades = JSON.parse(localStorage.getItem("upgrades")) || {
             goldenFingers: { cost: 50, boost: 1 },
             ironCookie: { cost: 250, boost: 5 },
             cookieGod: { cost: 1000, boost: 10 },
             cookieFactory: { cost: 5000, boost: 50 },
             cookieEmpire: { cost: 10000, boost: 100 }
         };
-        this.themes = {
+        this.themes = JSON.parse(localStorage.getItem("themes")) || {
             dark: {
                 cost: 1000,
                 background: "url('img/bg2.jpg')",
@@ -147,32 +157,6 @@ class CookieClickerGame {
                 color: "candyTheme"
             }
         };
-        this.startAutoClicker();
-        this.startClicksPerSecondCounter();
-        this.startSpecialEvents(); // Add this line
-        this.updateUI();
-    }
-
-    startSpecialEvents() {
-        setInterval(() => {
-            const eventChance = Math.random();
-            if (eventChance < 0.1) { // 10% chance for a special event
-                this.points += 1000; // Reward for the special event
-                alert("Special Event! You earned 1000 points!");
-                this.updateUI();
-                this.saveGame();
-            }
-        }, 60000); // Check for special events every 60 seconds
-    }
-
-    saveGame() {
-        localStorage.setItem("points", this.points);
-        localStorage.setItem("clickPower", this.clickPower);
-        localStorage.setItem("clicks", this.clicks);
-        localStorage.setItem("clicksPerSecond", this.clicksPerSecond);
-        localStorage.setItem("autoClickers", JSON.stringify(this.autoClickers));
-        localStorage.setItem("upgrades", JSON.stringify(this.upgrades));
-        localStorage.setItem("themes", JSON.stringify(this.themes));
     }
 
     resetGame() {
@@ -185,9 +169,9 @@ class CookieClickerGame {
             bakery: 0,
             factory: 0,
             mine: 0,
-            spaceship: 0, // New auto-clicker
-            robot: 0, // New auto-clicker
-            alien: 0 // New auto-clicker
+            spaceship: 0,
+            robot: 0,
+            alien: 0
         };
         this.upgrades = {
             goldenFingers: { cost: 50, boost: 1 },
@@ -247,6 +231,18 @@ class CookieClickerGame {
         }, 1000);
     }
 
+    startSpecialEvents() {
+        setInterval(() => {
+            const eventChance = Math.random();
+            if (eventChance < 0.1) { // 10% chance for a special event
+                this.points += 1000; // Reward for the special event
+                alert("Special Event! You earned 1000 points!");
+                this.updateUI();
+                this.saveGame();
+            }
+        }, 60000); // Check for special events every 60 seconds
+    }
+
     updateUI() {
         document.getElementById("pointsDisplay").innerText = this.points;
         document.getElementById("clickPower").innerText = this.clickPower;
@@ -258,9 +254,9 @@ class CookieClickerGame {
         document.getElementById("autoclickers-2").innerText = `Bakery: ${this.autoClickers.bakery}`;
         document.getElementById("autoclickers-3").innerText = `Factory: ${this.autoClickers.factory}`;
         document.getElementById("autoclickers-4").innerText = `Mine: ${this.autoClickers.mine}`;
-        document.getElementById("autoclickers-5").innerText = `Spaceship: ${this.autoClickers.spaceship}`; // New auto-clicker
-        document.getElementById("autoclickers-6").innerText = `Robot: ${this.autoClickers.robot}`; // New auto-clicker
-        document.getElementById("autoclickers-7").innerText = `Alien: ${this.autoClickers.alien}`; // New auto-clicker
+        document.getElementById("autoclickers-5").innerText = `Spaceship: ${this.autoClickers.spaceship}`;
+        document.getElementById("autoclickers-6").innerText = `Robot: ${this.autoClickers.robot}`;
+        document.getElementById("autoclickers-7").innerText = `Alien: ${this.autoClickers.alien}`;
 
         document.getElementById("buyGoldenFingers").innerText = `Golden Fingers (+1 Click Power) ${this.upgrades.goldenFingers.cost} pts`;
         document.getElementById("buyIronCookie").innerText = `Iron Cookie (+5 Click Power) ${this.upgrades.ironCookie.cost} pts`;
@@ -278,18 +274,15 @@ class CookieClickerGame {
 document.addEventListener("DOMContentLoaded", () => {
     const game = new CookieClickerGame();
 
-    document.getElementById("cookie").addEventListener("click", () => game.clickCookie());
-    document.getElementById("resetGame").addEventListener("click", () => game.resetGame());
-
-    new Incr1Button("cookie");
+    new Incr1Button("cookie", game);
     new AutoClickerButton("buyCursor", "cursor", 10, game);
     new AutoClickerButton("buyGrandma", "grandma", 20, game);
     new AutoClickerButton("buyBakery", "bakery", 100, game);
     new AutoClickerButton("buyFactory", "factory", 500, game);
     new AutoClickerButton("buyMine", "mine", 2000, game);
-    new AutoClickerButton("buySpaceship", "spaceship", 5000, game); // New auto-clicker
-    new AutoClickerButton("buyRobot", "robot", 10000, game); // New auto-clicker
-    new AutoClickerButton("buyAlien", "alien", 20000, game); // New auto-clicker
+    new AutoClickerButton("buySpaceship", "spaceship", 5000, game);
+    new AutoClickerButton("buyRobot", "robot", 10000, game);
+    new AutoClickerButton("buyAlien", "alien", 20000, game);
 
     new UpgradeButton("buyGoldenFingers", "goldenFingers", 50, game);
     new UpgradeButton("buyIronCookie", "ironCookie", 250, game);
@@ -301,5 +294,7 @@ document.addEventListener("DOMContentLoaded", () => {
     new ThemesButton("buyLight", "light", 2000, game);
     new ThemesButton("buyCookie", "cookie", 3000, game);
     new ThemesButton("buyCandy", "candy", 4000, game);
-    new saveGameButton("saveGame");
+
+    document.getElementById("resetGame").addEventListener("click", () => game.resetGame());
+    document.getElementById("saveGame").addEventListener("click", () => game.saveGame());
 });
